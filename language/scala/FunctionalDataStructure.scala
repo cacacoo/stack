2740993fd66ab7a8e4427228a0e4f3228dce8e88
bart(권형신) <cacacoo@coupang.com>
2019-01-12 19:18:47 +0900

    python for data science

--- language/scala/FunctionalDataStructure.scala
+++ language/scala/FunctionalDataStructure.scala
@@ -7,12 +7,12 @@ object FunctionalDataStructure {
 
   def removeFirst[A](list: List[A]): List[A] = list match {
     case Nil => Nil
-    case first :: rest => rest
+    case _ :: rest => rest
   }
 
   def drop[A](list: List[A], n: Int): List[A] = list match {
     case Nil => Nil
-    case head :: rest => {
+    case _ :: rest => {
       if(n == 1) rest
       else drop(rest, n-1)
     }
@@ -21,6 +21,15 @@ object FunctionalDataStructure {
   def dropWhile[A](list: List[A])(f: A => Boolean): List[A] = list match {
     case head :: rest if f(head) => dropWhile(rest)(f)
     case _ => list
+  }
+
+  def map[A,B](as: List[A])(f: A => B): List[B] = as match {
+    case Nil => Nil
+    case head :: rest => List(f(head)) ::: map(rest)(f)
+  }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+
   }
 
 }
